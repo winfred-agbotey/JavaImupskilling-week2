63576ddd4589d1593ef9025a7e4b672df4480899
winfred-agbotey <winfred.mawuli-agbotey@amalitech.com>
2024-11-05 09:44:30 +0000

    complete mvc lab1

---
+++ mvc-lab1/src/main/java/com/example/mvclab1/MvcLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.mvclab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcLab1Application.class, args);
+	}
+
+}
---
+++ mvc-lab1/src/test/java/com/example/mvclab1/MvcLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvclab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
